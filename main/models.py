67b62439d2cfed20f50473565d517a141bc95896
kaihojun <kaihojun@naver.com>
2024-05-19 22:28:53 +0900

    05/19 22:28

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Affiliation(models.Model):
     name = models.TextField()
+    country = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -17,15 +18,22 @@ class Author(models.Model):
         db_table = 'author'
 
 
+class Keyword(models.Model):
+    keyword_name = models.CharField(max_length=200)
+
+    class Meta:
+        managed = False
+        db_table = 'keyword'
+
+
 class Paper(models.Model):
     search = models.CharField(max_length=50)
     title = models.TextField()
-    url = models.TextField()
+    url = models.TextField(unique=True)
     date = models.DateField()
     citations = models.IntegerField()
     publisher = models.CharField(max_length=20)
     abstract = models.TextField()
-    keywords = models.TextField()
 
     class Meta:
         managed = False
@@ -48,3 +56,12 @@ class PaperAuthor(models.Model):
     class Meta:
         managed = False
         db_table = 'paper_author'
+
+
+class PaperKeyword(models.Model):
+    paper = models.ForeignKey(Paper, models.DO_NOTHING)
+    keyword = models.ForeignKey(Keyword, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'paper_keyword'
--- main/views.py
+++ main/views.py
@@ -3,46 +3,10 @@ from .models import Paper, Affiliation
 from django.db import connection
 
 def show_paper_info(request):
-    # SQL 쿼리 실행
-    with connection.cursor() as cursor:
-        cursor.execute('''
-            SELECT 
-                paper.id, paper.search, paper.title, paper.url, paper.date, paper.citations,
-                paper.publisher, paper.abstract, paper.keywords, GROUP_CONCAT(author.name) AS author_names
-            FROM 
-                paper
-            INNER JOIN 
-                paper_author ON paper.id = paper_author.paper_id
-            INNER JOIN 
-                author ON paper_author.author_id = author.id
-            GROUP BY
-                paper.id;
-        ''')
-        # 결과 가져오기
-        rows = cursor.fetchall()
-
-    # 결과를 딕셔너리 형태로 변환
-    papers = []
-    for row in rows:
-        paper = {
-            'id': row[0],
-            'search': row[1],
-            'title': row[2],
-            'url': row[3],
-            'date': row[4],
-            'citations': row[5],
-            'publisher': row[6],
-            'abstract': row[7],
-            'keywords': row[8],
-            'author_names': row[9].split(',') if row[9] else []  # 쉼표로 구분된 문자열을 리스트로 변환
-        }
-        papers.append(paper)
-
+    papers = Paper.objects.all()
     paper_count = len(papers)
     affiliation_count = Affiliation.objects.values('name').distinct().count()
-
     return render(request, 'index.html', {
-        'papers': papers,
         'paper_count': paper_count,
-        'affiliation_count': affiliation_count,
+        'affiliation_count': affiliation_count
     })
