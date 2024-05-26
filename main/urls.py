adcb7da1cab1006cbe5912c26267a613f6ef0751
kaihojun <kaihojun@naver.com>
2024-05-26 17:49:59 +0900

    05/26 17:49

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import country_publications_graph, author_publications_graph, year_publications_graph, affiliation_publication_graph, keyword_counts_graph
 
 urlpatterns = [
-    path('main/', views.show_paper_info, name='main'),
+    path('', views.show_paper_info, name='main'),
     path('country-graph/', country_publications_graph, name='country_graph'),
     path('author-graph/', author_publications_graph, name='author_graph'),
     path('year-graph/', year_publications_graph, name='year_graph' ),
