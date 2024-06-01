51b3cb5216dd9d910e37bcf750e0ff984afab54a
kaihojun <kaihojun@naver.com>
2024-06-01 17:53:01 +0900

    06/01 17:52

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
-from .views import country_publications_graph, author_publications_graph, year_publications_graph, affiliation_publication_graph, keyword_counts_graph
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('analyze/', views.analyze, name='total_graph'),
-    path('country-graph/', country_publications_graph, name='country_graph'),
-    path('author-graph/', author_publications_graph, name='author_graph'),
-    path('year-graph/', year_publications_graph, name='year_graph' ),
-    path('affiliation-graph/', affiliation_publication_graph, name='affiliation_graph' ),
-    path('keyword-graph/', keyword_counts_graph, name='keyword_graph' ),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from .models import Paper, Affiliation
 from django.db import connection
 from collections import Counter
+from datetime import datetime
+from django.core.paginator import Paginator
+
+
 
 db_config = {
     'host': 'localhost',
@@ -21,14 +25,39 @@ def home(request):
         'affiliation_count': affiliation_count
     })
 
+# 검색 시 논문 출력 및 필터링
 def search(request):
     query = request.GET.get('query', '')
-    # 여기에 검색 결과 처리 로직을 추가할 수 있습니다.
+    year = request.GET.get('year', '')
+
+    paper_ids = get_paper_ids(query)
+    print(paper_ids)
+    
+    if year:
+        # 연도 필터를 사용하여 필터링
+        papers = Paper.objects.filter(id__in=paper_ids, date__year=year)
+    else:
+        papers = Paper.objects.filter(id__in=paper_ids)
+
+    # paper_ids 리스트 순서대로 정렬
+    ordered_papers = sorted(papers, key=lambda paper: paper_ids.index(paper.id))
+
+    # 페이징
+    paginator = Paginator(ordered_papers, 20)  # 페이지당 20개씩 표시
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # 연도별 논문 수 계산
+    years = range(2020, datetime.now().year + 1)
+    paper_counts_by_year = {str(y): Paper.objects.filter(id__in=paper_ids, date__year=y).count() for y in years}
+
     context = {
         'query': query,
-        # 다른 컨텍스트 데이터 추가
+        'papers': page_obj,
+        'paper_counts_by_year': paper_counts_by_year,
     }
     return render(request, 'search.html', context)
+
 
 # 검색 엔진
 def get_paper_ids(user_keyword):
@@ -74,194 +103,7 @@ def get_paper_ids(user_keyword):
 
     return paper_ids
 
-# 나라별 논문 수 그래프
-def country_publications_graph(request):
-    if request.method == 'GET' and 'query' in request.GET:
-        user_keyword = request.GET['query']
-        paper_ids = get_paper_ids(user_keyword)
-
-        # 국가별 논문 수 계산
-        country_counts = []
-        if paper_ids:
-            placeholders = ', '.join(['%s'] * len(paper_ids))
-            with connection.cursor() as cursor:
-                cursor.execute(f"""
-                SELECT c.name AS country_name, COUNT(pc.paper_id) AS publications_count
-                FROM country c
-                JOIN paper_country pc ON c.id = pc.country_id
-                JOIN paper p ON pc.paper_id = p.id
-                WHERE p.id IN ({placeholders})
-                GROUP BY c.name
-                ORDER BY publications_count DESC
-                LIMIT 10;
-                """, paper_ids)
-                
-                country_counts = cursor.fetchall()
-
-        # 국가 이름과 논문 수를 결합한 리스트 생성
-        country_data = list(zip([item[0] for item in country_counts], [item[1] for item in country_counts]))
-
-        # 그래프에 사용할 데이터 준비
-        context = {
-            'country_data': country_data,  # 국가 이름과 논문 수를 결합한 리스트
-            'keyword': user_keyword  # 검색된 키워드
-        }
-
-        return render(request, 'country_graph.html', context)
-    else:
-        return render(request, 'error.html', {'message': 'Invalid request'})
-
-
-# 저자별 논문 수 그래프
-def author_publications_graph(request):
-    if 'query' in request.GET:
-        user_keyword = request.GET['query']
-        paper_ids = get_paper_ids(user_keyword)
-
-        # 저자별 논문 수를 계산
-        author_counts = []
-        if paper_ids:
-            placeholders = ', '.join(['%s'] * len(paper_ids))  # placeholders 생성
-            with connection.cursor() as cursor:
-                cursor.execute(f"""
-                SELECT a.name AS author_name, COUNT(pa.paper_id) AS publications_count
-                FROM author a
-                JOIN paper_author pa ON a.id = pa.author_id
-                JOIN paper p ON pa.paper_id = p.id
-                WHERE p.id IN ({placeholders})
-                GROUP BY a.name
-                ORDER BY publications_count DESC
-                LIMIT 10;
-                """, paper_ids)
-
-                author_counts = cursor.fetchall()
-
-        # 저자 이름과 논문 수를 결합한 리스트 생성
-        author_data = list(zip([item[0] for item in author_counts], [item[1] for item in author_counts]))
-
-        # 그래프에 사용할 데이터 준비
-        context = {
-            'author_data': author_data,  # 저자 이름과 논문 수를 결합한 리스트
-            'keyword': user_keyword  # 검색된 키워드
-        }
-
-        return render(request, 'author_graph.html', context)
-    else:
-        return render(request, 'error.html', {'message': 'Invalid request'})
-
-# 연도별 논문 증감 그래프
-def year_publications_graph(request):
-    if 'query' in request.GET:
-        user_keyword = request.GET['query']
-        paper_ids = get_paper_ids(user_keyword)
-
-        # MariaDB 데이터베이스 연결
-        db = mariadb.connect(**db_config)
-        # 커서 생성
-        cursor = db.cursor(dictionary=True)
-
-        # 연도별로 그룹화하고 각 연도의 논문 수 구하기
-        if paper_ids:
-            placeholders = ', '.join(['%s'] * len(paper_ids))
-            cursor.execute(f"""
-            SELECT YEAR(p.date) AS year, COUNT(*) AS count
-            FROM paper p
-            WHERE p.id IN ({placeholders})
-            GROUP BY year
-            ORDER BY year;
-            """, paper_ids)
-            
-            papers_count = cursor.fetchall()
-        else:
-            papers_count = []
-
-        # 데이터베이스 연결 종료
-        cursor.close()
-        db.close()
-
-        # 그래프에 사용할 데이터 준비
-        context = {'papers_count': papers_count}
-
-        return render(request, 'year_graph.html', context)
-    else:
-        return render(request, 'error.html', {'message': 'Invalid request'})
-
-
-# 소속별 논문 수 그래프
-def affiliation_publication_graph(request):
-    if 'query' in request.GET:
-        user_keyword = request.GET['query']
-        paper_ids = get_paper_ids(user_keyword)
-
-        # 소속별 논문 수를 계산
-        affiliation_counts = []
-        if paper_ids:
-            placeholders = ', '.join(['%s'] * len(paper_ids))  # placeholders 생성
-            with connection.cursor() as cursor:
-                cursor.execute(f"""
-                SELECT af.name AS affiliation_name, COUNT(pa.paper_id) AS publications_count
-                FROM affiliation af
-                JOIN paper_affiliation pa ON af.id = pa.affiliation_id
-                JOIN paper p ON pa.paper_id = p.id
-                WHERE p.id IN ({placeholders})
-                GROUP BY af.name
-                ORDER BY publications_count DESC
-                LIMIT 10;
-                """, paper_ids)
-
-                affiliation_counts = cursor.fetchall()
-
-        # 소속 이름과 논문 수를 결합한 리스트 생성
-        affiliation_data = list(zip([item[0] for item in affiliation_counts], [item[1] for item in affiliation_counts]))
-
-        # 그래프에 사용할 데이터 준비
-        context = {
-            'affiliation_data': affiliation_data,  # 소속 이름과 논문 수를 결합한 리스트
-            'keyword': user_keyword  # 검색된 키워드
-        }
-
-        return render(request, 'affiliation_graph.html', context)
-    else:
-        return render(request, 'error.html', {'message': 'Invalid request'})
-
-
-# 검색어에 대한 TOP 10 키워드
-def keyword_counts_graph(request):
-    if 'query' in request.GET:
-        user_keyword = request.GET['query']
-        paper_ids = get_paper_ids(user_keyword)
-
-        # 키워드 카운트 초기화
-        keyword_counts = Counter()
-
-        if paper_ids:
-            placeholders = ', '.join(['%s'] * len(paper_ids))  # placeholders 생성
-            with connection.cursor() as cursor:
-                cursor.execute(f"""
-                SELECT k.keyword_name
-                FROM paper_keyword pk
-                JOIN keyword k ON pk.keyword_id = k.id
-                WHERE pk.paper_id IN ({placeholders});
-                """, paper_ids)
-
-                # 쿼리 결과 가져오기
-                keywords = [row[0] for row in cursor.fetchall()]
-                keyword_counts.update(keywords)
-
-        # 빈도수가 높은 top 10 키워드 출력하기
-        top_keywords = keyword_counts.most_common(10)
-
-        # 그래프에 사용할 데이터 준비
-        context = {
-            'top_keywords': top_keywords,  # 상위 10개의 키워드와 그 빈도수
-            'keyword': user_keyword  # 검색된 키워드
-        }
-
-        return render(request, 'keyword_graph.html', context)
-    else:
-        return render(request, 'error.html', {'message': 'Invalid request'})
-    
-
+# 분석 페이지 그래프
 def analyze(request):
     if 'query' in request.GET:
         user_keyword = request.GET['query']
@@ -297,7 +139,7 @@ def analyze(request):
             placeholders = ', '.join(['%s'] * len(paper_ids))  # placeholders 생성
             with connection.cursor() as cursor:
                 cursor.execute(f"""
-                SELECT a.name AS author_name, COUNT(pa.paper_id) AS publications_count
+                SELECT a.name AS author_name, COUNT(distinct(pa.paper_id)) AS publications_count
                 FROM author a
                 JOIN paper_author pa ON a.id = pa.author_id
                 JOIN paper p ON pa.paper_id = p.id
@@ -319,7 +161,7 @@ def analyze(request):
             placeholders = ', '.join(['%s'] * len(paper_ids))  # placeholders 생성
             with connection.cursor() as cursor:
                 cursor.execute(f"""
-                SELECT af.name AS affiliation_name, COUNT(pa.paper_id) AS publications_count
+                SELECT af.name AS affiliation_name, COUNT(distinct(pa.paper_id)) AS publications_count
                 FROM affiliation af
                 JOIN paper_affiliation pa ON af.id = pa.affiliation_id
                 JOIN paper p ON pa.paper_id = p.id
@@ -342,7 +184,7 @@ def analyze(request):
             placeholders = ', '.join(['%s'] * len(paper_ids))
             with connection.cursor() as cursor:
                 cursor.execute(f"""
-                SELECT c.name AS country_name, COUNT(pc.paper_id) AS publications_count
+                SELECT c.name AS country_name, COUNT(distinct(pc.paper_id)) AS publications_count
                 FROM country c
                 JOIN paper_country pc ON c.id = pc.country_id
                 JOIN paper p ON pc.paper_id = p.id
@@ -389,3 +231,67 @@ def analyze(request):
         }
 
         return render(request, 'total_graph.html', context)
+    
+
+# Django orm 사용
+# from collections import Counter
+# from django.db.models import Count
+# from django.shortcuts import render
+# from .models import Paper, Author, Affiliation, Country, Keyword
+
+# def analyze(request):
+#     if 'query' in request.GET:
+#         user_keyword = request.GET['query']
+#         paper_ids = get_paper_ids(user_keyword)
+
+#         papers_count = []
+#         if paper_ids:
+#             papers_count = (Paper.objects
+#                             .filter(id__in=paper_ids)
+#                             .values('date__year')
+#                             .annotate(count=Count('id'))
+#                             .order_by('date__year'))
+
+#         author_counts = []
+#         if paper_ids:
+#             author_counts = (Author.objects
+#                              .filter(paperauthor__paper_id__in=paper_ids)
+#                              .values('name')
+#                              .annotate(publications_count=Count('paperauthor__paper'))
+#                              .order_by('-publications_count')[:10])
+
+#         affiliation_counts = []
+#         if paper_ids:
+#             affiliation_counts = (Affiliation.objects
+#                                   .filter(paperaffiliation__paper_id__in=paper_ids)
+#                                   .values('name')
+#                                   .annotate(publications_count=Count('paperaffiliation__paper'))
+#                                   .order_by('-publications_count')[:10])
+
+#         country_counts = []
+#         if paper_ids:
+#             country_counts = (Country.objects
+#                               .filter(papercountry__paper_id__in=paper_ids)
+#                               .values('name')
+#                               .annotate(publications_count=Count('papercountry__paper'))
+#                               .order_by('-publications_count')[:10])
+
+#         keyword_counts = Counter()
+#         if paper_ids:
+#             keywords = (Keyword.objects
+#                         .filter(paperkeyword__paper_id__in=paper_ids)
+#                         .values_list('keyword_name', flat=True))
+#             keyword_counts.update(keywords)
+
+#         top_keywords = keyword_counts.most_common(10)
+
+#         context = {
+#             'papers_count': list(papers_count),
+#             'author_data': [(item['name'], item['publications_count']) for item in author_counts],
+#             'affiliation_data': [(item['name'], item['publications_count']) for item in affiliation_counts],
+#             'country_data': [(item['name'], item['publications_count']) for item in country_counts],
+#             'top_keywords': top_keywords,
+#             'keyword': user_keyword
+#         }
+
+#         return render(request, 'total_graph.html', context)
